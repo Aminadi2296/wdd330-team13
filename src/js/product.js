64502909caf640098abad967b77688e7b50350bc
Moroni Perez <66393582+Aminadi2296@users.noreply.github.com>
2025-03-25 23:37:23 -0400

    Update product links and refactor product page for improved clarity and functionality

--- src/js/product.js
+++ src/js/product.js
@@ -1,28 +1,14 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
+
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  // Step 1: Retrieve cart items from localStorage
-  const cartItems = getLocalStorage("so-cart") || [];
-  console.log("Retrieved Cart Items (Before Update):", cartItems);
 
-  // Step 2: Add the new product to the cart
-  cartItems.push(product);
-  console.log("Updated Cart Items (After Update):", cartItems);
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-  // Step 3: Save the updated cart back to localStorage
-  setLocalStorage("so-cart", cartItems);
-  console.log("Cart Items Saved to LocalStorage:", getLocalStorage("so-cart"));
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+
