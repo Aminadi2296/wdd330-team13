190692f00628cd679724cc46016381ef5bbf4d35
Moroni Perez <66393582+Aminadi2296@users.noreply.github.com>
2025-04-08 23:49:32 -0400

    Add checkout functionality: implement checkout process, remove old checkout page, and update cart page with checkout link

---
+++ src/js/checkout.js
@@ -0,0 +1,20 @@
+import CheckoutProcess from "./CheckoutProcess.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
+
+const order = new CheckoutProcess("so-cart", ".checkout-summary");
+order.init();
+
+document
+  .querySelector("#zip")
+  .addEventListener("blur", order.calculateOrderTotal.bind(order));
+
+document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+  e.preventDefault();
+  const myForm = document.forms[0];
+  const chk_status = myForm.checkValidity();
+  myForm.reportValidity();
+  if(chk_status)
+    order.checkout();
+});
